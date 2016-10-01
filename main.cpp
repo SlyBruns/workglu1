9b86a1850964d59988c2a53ff69a0a73b464b308
Vlad Bruns <brunsvd@gmail.com>
2016-10-01 05:50:15 +0000

    PR1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
-#include <stdout>
+
+using namespace std;
 
 int main() {
-    double x, y, f;
-    for(int i=1,i<100000, i++ )
+    const float e = 0.000001;
+    for(int i=1; i<100000; i++ )
     {
+        double s1, olds;
+        double fp = 1, fpp = 2, f, fact = 2, n = 3;
         
+        do
+        {
+            if (fp==1)
+            {
+                s1 = fp/fact;
+            } else
+            {
+                s1 = olds + fp/fact;   
+            }
+            
+            olds = s1;
+            f = fpp;
+            fpp = fp + fpp;
+            fp = f;
+          
+            f = fpp;
+            fpp = fp + fpp;
+            fp = f;
+          
+            fact = fact * n;
+            n++;
+          
+            fact = fact * n;
+            n++; 
+        }
+        while ( e);
     }
 }
